1ca21e4af6d2354a2a1ba8ab0cc7811601ff7d67
Juan Tevez <juan.tevez@gmail.com>
2026-06-01 03:58:58 -0300

    fix local deploy

--- deploy/docker/init-multiple-databases.sh
+++ deploy/docker/init-multiple-databases.sh
@@ -2,13 +2,10 @@
 set -e
 
 echo "🐳 Creando bases de datos adicionales..."
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE auth_db;
-    GRANT ALL PRIVILEGES ON DATABASE auth_db TO $POSTGRES_USER;
-
-    CREATE DATABASE inmo_catalog_db;
-    GRANT ALL PRIVILEGES ON DATABASE inmo_catalog_db TO $POSTGRES_USER;
-
     CREATE DATABASE inmo_maintenance_db;
-    GRANT ALL PRIVILEGES ON DATABASE inmo_maintenance_db TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE auth_db TO inmo_user;
+    GRANT ALL PRIVILEGES ON DATABASE inmo_maintenance_db TO inmo_user;
 EOSQL
